85bbb6a50ae026a3ef924abde39ead2d5def8c4d
StudyCool <as.shevh@gmail.com>
2020-02-26 11:30:57 +0200

    add components

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -3,7 +3,7 @@ import styles from './Display.module.css';
 
 function Display (props) {
   return (
-    <div className={styles.display}>{props.children}</div>
+    <div className={styles.display}>ON</div>
   )
 }
 
